905a78908ebd66a3a745053c40139b482cbd6499
Jason Blanchard <jas.blanchard@gmail.com>
2016-01-13 19:34:01 -0500

    Working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,28 @@
-console.log('hello');
+function showNextImage() {
+  var visibleIndex = $('img').get().indexOf($('img:visible')[0]);
+  var nextIndex = (visibleIndex + 1) % $('img').length;
+  $($('img').get(visibleIndex)).hide();
+  $($('img').get(nextIndex)).show();
+}
+
+var interval;
+
+$(document).ready(function() {
+
+  $('img').hide();
+  $('img').first().show();
+
+  $('img').on('click', function() {
+    showNextImage();
+  });
+
+  $('.play').on('click', function() {
+    interval = setInterval(function() {
+      showNextImage();
+    }, 1000);
+  });
+
+  $('.stop').on('click', function() {
+    clearInterval(interval);
+  });
+});
